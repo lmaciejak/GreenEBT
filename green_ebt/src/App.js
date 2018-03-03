47b5e74eb871d9cf6ed620d5d9762354062525d2
Ivan <imendoza9691@gmail.com>
2018-03-03 16:56:16 -0500

    #8 completed MarketInfo

--- green_ebt/src/App.js
+++ green_ebt/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Map from './components/Map.js'
+import Map from './components/Map.js';
+import MarketInfo from "./components/MarketInfo.js";
 
 
 class App extends Component {
@@ -14,13 +15,20 @@ class App extends Component {
   };
 
   render() {
-    return (
-      <div className="App">
+    const { selectedMarket } = this.state;
+    console.log("selected: ", selectedMarket);
+    return <div className="App">
         <div id="map-container">
-        <Map onMarketClick={this.onMarketClick} />
-      </div>
-      </div>
-    );
+          <Map onMarketClick={this.onMarketClick} />
+        </div>
+        <div id="market-info">
+          {selectedMarket ? (
+            MarketInfo(selectedMarket)
+          ) : (
+            <strong> Choose a farmers' market </strong>
+          )}
+        </div>
+      </div>;
   }
 }
 
