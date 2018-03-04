4ba777fb3c0b5485e5e0783d130c3c5c6faa6ae3
princessguerrero <princessguerrero@ac.c4q.nyc>
2018-03-03 22:33:10 -0500

    combined map and selectBorough in a component

--- green_ebt/src/App.js
+++ green_ebt/src/App.js
@@ -1,46 +1,26 @@
-import { Link, Switch, Route } from 'react-router-dom';
+import { Link, Switch, Route } from "react-router-dom";
 import About from "./components/About";
 import SelectBorough from "./components/SelectBorough";
-import React, { Component } from 'react';
-import './App.css';
-import Map from './components/Map.js';
+import React, { Component } from "react";
+import "./App.css";
+import Map from "./components/Map.js";
 import MarketInfo from "./components/MarketInfo.js";
+import MapAndBoro from "./components/MapAndBoro.js";
 
 class App extends Component {
-  state = {
-    selectedMarket: null
-  };
-
-  onMarketClick = market => {
-    this.setState({ selectedMarket: market });
-  };
-
   render() {
-    const { selectedMarket } = this.state;
-    console.log("selected: ", selectedMarket);
     return (
-      <div className="App">
+      <div>
         <nav>
-          <Link to="/">Map</Link>
+          <Link to="/">Map</Link> {"  "}
           <Link to="/about">About Us</Link>
         </nav>
-       <Switch>
-          <Route exact path="/" component={SelectBorough} />
+        <Switch>
+          <Route exact path="/" component={MapAndBoro} />
           <Route path="/about" component={About} />
         </Switch>
-
-      <div id="map-container">
-          <Map onMarketClick={this.onMarketClick} />
-        </div>
-        <div id="market-info">
-          {selectedMarket ? (
-            MarketInfo(selectedMarket)
-          ) : (
-            <strong> Choose a farmers' market </strong>
-          )}
-        </div>
       </div>
-);
+    );
   }
 }
 
